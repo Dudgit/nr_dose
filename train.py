da582040c55ec409c7abdaa615fca4afe83e10bb
Dudgit <bence.dudas20@gmail.com>
2026-07-06 18:31:56 +0000

    train_naming_debugged

--- train.py
+++ train.py
@@ -89,6 +89,7 @@ def train_dota():
     run_name_base = cfg['atlasz_run_name'] if args.hw == "atlasz" else cfg['komondor_run_name']
     run_name_base = run_name_base + "_adv" if cfg['train']['adversarial']['use'] else run_name_base
     modelname = cfg['atlasz_modelname'] if args.hw == "atlasz" else cfg['komondor_modelname']
+    cfg['modelname'] = modelname
     run_name = args.hw + "_" + run_name_base + "_" + str(modelname) + "_" + str(cfg['train']['num_epochs']) + "epochs"
 
     train_loader, val_loader = get_loaders(hw = args.hw)
