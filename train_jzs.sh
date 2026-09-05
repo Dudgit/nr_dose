425a0ea52729e4f0c030ad8ba7f52af931ee860b
Dudás Bence <52243953+Dudgit@users.noreply.github.com>
2026-09-05 19:49:32 +0200

    Remove WANDB API key export from script

--- train_jzs.sh
+++ train_jzs.sh
@@ -7,7 +7,6 @@
 #SBATCH --time=4:00:00
 #SBATCH --output=logs/new_ener.log
 
-export SINGULARITYENV_WANDB_API_KEY="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"
 export SCRATCH="/home/nr_dodb/nr_dose_scratch"
 export REAL_SCRATCH=$(readlink -f $SCRATCH)
 
